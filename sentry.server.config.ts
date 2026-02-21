3e922b164d58dee472fbaab9ed703c5bf930f9e7
Jaseem <muhammedjaseemvk96@gmail.com>
2026-02-21 12:17:23 +0530

    Create header, add Sentry and metadata

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ce653b50f6c4f8cf05a66b73e5b107ff@o4510919680851968.ingest.us.sentry.io/4510919684980736",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
---
+++ src/app/(rs)/home/page.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "Page Not Found",
+};
+
+export default function Home(){
+    return <h2>Home Page</h2>
+}
